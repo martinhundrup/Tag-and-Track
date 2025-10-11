8af46dc1a20f3fae5090483133533088ae9c36c1
martinhundrup <hundrupm@gmail.com>
2025-10-11 13:20:02 -0700

    debug text to console when scanning

--- code/TagAndTrack/TagAndTrack/Pages/ScanItemPage.cs
+++ code/TagAndTrack/TagAndTrack/Pages/ScanItemPage.cs
@@ -11,7 +11,14 @@ namespace TagAndTrack.Pages
         {
             Title = titleText;
 
+            var scanView = new ScanView();
+            scanView.ScanCaptured += ScanCaptured;
             Content = new ScanView();
+        }
+
+        private void ScanCaptured(object? sender, ScanCapturedEventArgs args)
+        {
+            Console.WriteLine($"scan captured: {args.Text}");
         }
     }
 }
