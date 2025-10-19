b7e41beb89e9186b0e596929b065bfbfe1cc9c2e
martinhundrup <hundrupm@gmail.com>
2025-10-18 17:37:43 -0700

    added item classes

---
+++ code/TagAndTrack/TagAndTrack/Items/ContainerItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagAndTrack.Items
+{
+    internal class ContainerItem : Item
+    {
+        public ContainerItem() {
+            Type = ItemType.Container;
+        }
+
+        public ContainerItem(string name, string description) : this()
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ code/TagAndTrack/TagAndTrack/Items/Item.cs
@@ -0,0 +1,63 @@
+﻿namespace TagAndTrack.Items
+{
+
+    internal abstract class Item
+    {
+        public enum ItemType
+        {
+            None,           // for errors
+            Specimen,
+            Loan,           // contains multiple specimens and loan info
+            Container,      // for shelves and stuff
+        }
+
+        public Item()
+        {
+            Type = Item.ItemType.None;
+        }
+
+        public Item(string name, string description) : this()
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public ItemType Type // avoids using reflection
+        {
+            get;
+            protected set;
+        }
+
+        public int ID // internal ID to tag and track
+        {
+            get;
+            protected set;
+        }
+
+        public string? ArctosID // ArctosDB ID
+        {
+            get;
+            protected set;
+        }
+
+        public string? Name // display name
+        {
+            get;
+            protected set;
+        }
+
+        public string? Description // brief description of the item
+        {
+            get;
+            protected set;
+        }
+
+        public string? QRID // qr code ID
+        {
+            get
+            {
+                return $"{Type.ToString()}:{ID}"; // ex: "Specimen:4920382"
+            }
+        }
+    }
+}
