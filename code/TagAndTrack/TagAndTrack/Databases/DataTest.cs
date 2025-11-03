ebe3db5bf5bdcc6f2900a42e972a82d5d53ce010
swill <shawn.will@wsu.edu>
2025-11-03 15:05:17 -0800

    basic SQLite database test classes implemented

---
+++ code/TagAndTrack/TagAndTrack/Databases/DataTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TagAndTrack.Databases
+{
+    public class DataTest
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(100)]
+        public string ARC_ID { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Description { get; set; } = string.Empty;
+
+        public bool Present { get; set; }
+    }
+}
---
+++ code/TagAndTrack/TagAndTrack/Databases/DatabaseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TagAndTrack.Databases
+{
+    public class DatabaseTest
+    {
+        private readonly SQLiteConnection testDatabase;
+
+        public DatabaseTest()
+        {
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "testData.db3");
+
+            testDatabase = new SQLiteConnection(filePath);
+            testDatabase.CreateTable<DataTest>();
+        }
+
+        public void AddData(string arc_ID, string name, string desc, bool present)
+        {
+            var data = new DataTest { ARC_ID = arc_ID, Name = name, Description = desc, Present = present };
+            testDatabase.Insert(data);
+        }
+
+        public List<DataTest> GetListOfData()
+        {
+            return testDatabase.Table<DataTest>().ToList();
+        }
+
+        public DataTest? FindDataByID(int id)
+        {
+            return testDatabase.Table<DataTest>().FirstOrDefault(data => data.ID == id);
+        }
+
+        public DataTest? FindDataByName(string name)
+        {
+            return testDatabase.Table<DataTest>().FirstOrDefault(data => data.Name == name);
+        }
+
+        public void RemoveData(int id)
+        {
+            testDatabase.Delete(id);
+        }
+    }
+}
