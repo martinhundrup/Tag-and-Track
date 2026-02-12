a52c192f7b4be1c5b25e091b2048bdf9d0be0790
Stellarone60 <sarah.carley@hotmail.com>
2026-02-12 10:51:30 -0800

    Fixed file naming conventions

---
+++ code/TagAndTrack/TagAndTrack/Components/SpecimenDataTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagAndTrack.Components
+{
+    /// <summary>
+    /// Class for a data table that will contain specimens.
+    /// </summary>
+    public class SpecimenDataTable
+    {
+    }
+}
